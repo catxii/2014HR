3db86a664c793ecb35b31c00dfd86559c3033c04
catxii <catxii@catxii-MacBook-Pro.local>
2014-07-26 17:51:16 +0800

    修复公共页面

--- space-css.js
+++ space-css.js
@@ -1,7 +1,5 @@
 // 2014“暑假班”样式公用库 
 
-
-
 function loadJS(filename){
 	for (var i = 0; i < filename.length; i++) {
 		document.write(filename[i]);
@@ -20,7 +18,21 @@ var filegroup = [
 	"<link href='libs/select2/select2.css' rel='stylesheet'>",
 
 	// Custom styles for this template 
-	"<link href='css/style-v1.css' rel='stylesheet'>"
+	"<link href='css/style-v1.css' rel='stylesheet'>",
+
+	// Just for debugging purposes. Don't actually copy these 2 lines! 
+	"<!--[if lt IE 9]> <script src='libs/bootstrap/js/ie8-responsive-file-warning.js'></script> <![endif]-->",
+	"<script src='libs/bootstrap/js/ie-emulation-modes-warning.js'></script>",
+
+	// IE10 viewport hack for Surface/desktop Windows 8 bug 
+	"<script src='libs/bootstrap/js/ie10-viewport-bug-workaround.js'></script>",
+
+	// HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries 
+	"<!--[if lt IE 9]>\
+	<script src='https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js'></script>\
+	<script src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js'>\
+	</script>\
+	<![endif]-->"
 
 ]
 
