57e0e319e570f38f13976424e41807cb182f61d8
catxii <catxii@catxii-MacBook-Pro.local>
2014-08-24 17:41:15 +0800

    修改样式

--- space-css.js
+++ space-css.js
@@ -25,18 +25,18 @@ var filegroup = [
     "<link href='css/style-feng.css' rel='stylesheet'>", 
 
 	// Just for debugging purposes. Don't actually copy these 2 lines! 
-	"<!--[if lt IE 9]> <script src='libs/bootstrap/js/ie8-responsive-file-warning.js'></script> <![endif]-->",
-	"<script src='libs/bootstrap/js/ie-emulation-modes-warning.js'></script>",
+	// "<!--[if lt IE 9]> <script src='libs/bootstrap/js/ie8-responsive-file-warning.js'></script> <![endif]-->",
+	// "<script src='libs/bootstrap/js/ie-emulation-modes-warning.js'></script>",
 
 	// IE10 viewport hack for Surface/desktop Windows 8 bug 
 	"<script src='libs/bootstrap/js/ie10-viewport-bug-workaround.js'></script>",
 
 	// HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries 
-	"<!--[if lt IE 9]>\
-	<script src='https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js'></script>\
-	<script src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js'>\
-	</script>\
-	<![endif]-->",
+	// "<!--[if lt IE 9]>\
+	// <script src='https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js'></script>\
+	// <script src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js'>\
+	// </script>\
+	// <![endif]-->",
 
 	//当为IE6~9的
 	"<!--[if IE 6]>\
